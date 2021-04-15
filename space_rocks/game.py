3b35b0a1612603d1c1929b5ca318375dd2ecf595
Pouria <user@users-MacBook-Pro.local>
2021-04-15 09:41:40 -0700

    add spaceship and asteroid gameobjects

--- space_rocks/game.py
+++ space_rocks/game.py
@@ -1,6 +1,7 @@
 import pygame
 
 from utils import load_sprite
+from models import GameObject
 
 
 class SpaceRocks:
@@ -8,6 +9,8 @@ class SpaceRocks:
 		self._init_pygame()
 		self.screen = pygame.display.set_mode((800, 600))
 		self.background = load_sprite("space", False)
+		self.spaceship = GameObject((400, 300), load_sprite("spaceship"), (0, 0))
+		self.asteroid = GameObject((400, 300), load_sprite("asteroid"), (1, 0))
 
 	def main_loop(self):
 		while True:
@@ -27,8 +30,11 @@ class SpaceRocks:
 				quit()
 
 	def _process_game_logic(self):
-		pass
+		self.spaceship.move()
+		self.asteroid.move()
 
 	def _draw(self):
 		self.screen.blit(self.background, (0, 0))
+		self.spaceship.draw(self.screen)
+		self.asteroid.draw(self.screen)
 		pygame.display.flip()
