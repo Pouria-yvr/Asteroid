d3965901f73e20a92d9a5e26bf0bb6f6886f329a
Pouria <user@users-MacBook-Pro.local>
2021-04-25 18:30:54 -0700

    Add rotation and draw to spaceship class

--- space_rocks/models.py
+++ space_rocks/models.py
@@ -1,6 +1,7 @@
 from pygame.math import Vector2
 from utils import load_sprite
 
+UP = Vector2(0, -1)
 
 class GameObject:
 	def __init__(self, position, sprite, velocity):
@@ -21,5 +22,14 @@ class GameObject:
 		return distance < self.radius + other_obj.radius
 
 class SpaceShip(GameObject):
+	# Angle in degrees that obj direction can rotate in each frame
+	MANEUVERABILITY = 3
 	def __init__(self, position):
+		# Make a copy of the original UP vector
+		self.direction = Vector2(UP)
 		super().__init__(position, load_sprite("spaceship"), Vector2(0))
+
+	def rotate(self, clockwise=True):
+		sign = 1 if clockwise else -1
+		amgle = self.MANEUVERABILITY * sign
+		self.direction.rotate_ip(angle)
